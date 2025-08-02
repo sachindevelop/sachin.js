d5c209fe6b6d0856ca3de65334cdb471d4a96abc
sachindevelop <rs6555576@gmail.com>
2025-08-02 06:48:27 +0530

    untrack files

---
+++ demo.js
@@ -0,0 +1,154 @@
+/*function area(l,b)
+{ 
+    var a=l*b
+    console.log("area is:"+a)
+}
+var l=10
+var b=20
+area(10,20)*/
+
+/*function myname()
+{
+    return "sachin"
+}
+var a=myname()
+console.log(a)*/
+
+/*function add(a,b)
+{
+    return(a+b)
+}
+var add=add(10,20)
+console.log(add)*/
+
+/*var rain=false
+ if(rain)
+ {
+    console.log("take an umberala")
+ }
+ else
+ {
+    console.log("enjoy the rain")
+ }*/
+
+/*var homeworkfinish=false
+if (homeworkfinish)
+{
+    console.log("great job")
+}
+else
+{
+    console.log("finish your homework before playing")
+}*/
+
+/*var anycookiesleft=false
+if(anycookiesleft) 
+   {
+       console.log("would you like a cookie?")
+   }
+   else
+       {
+           console.log("time to bake more cookie")
+       } */
+           
+/*var brushteeth=false
+if(brushteeth)
+{
+ console.log("your teeth is clean")
+}
+else{
+    console.log("please clean your teeth ")
+}*/
+
+/*var birthday=false
+if(birthday){
+    console.log("happy birthday enjoy special day")
+}
+else{
+    console.log("have a great day")
+}*/
+
+/*var seasonyear="winter"
+if(seasonyear=="spring")
+{
+    console.log("enjoy the blooming flowers")
+}
+else if(seasonyear=="summer")
+{
+    console.log("have fun in the sun")
+}
+else if(seasonyear=="autumn")
+{
+    console.log("admire the colorful leaves")
+}
+else if(seasonyear=="winter")
+{
+    console.log("bundle up and stay warm")
+}*/
+
+/*var number="odd"
+if(number=="even")
+{
+    console.log("the number is even")
+}
+else if(number=="odd")
+{
+    console.log("the number is odd")
+}*/
+
+/*var character="constant"
+if(character=="vowel")
+{
+    console.log("it is vowel")
+}
+else if(character=="constant")
+{
+    console.log("it is constant")
+}*/
+
+/*for(count=1;count<=6;count=count+1)
+{
+    console.log("sachin")
+}*/
+
+/*for(count=1;count<=10;count=count+1)
+{
+    console.log(count)
+}*/
+
+/*for(count=1;count<=10;count=count+2)
+{
+    console.log(count)
+}*/
+
+/*for(count=10;count>=1;count=count-1)
+{
+    console.log(count)
+}*/
+
+/*for(count=2;count<=10;count=count+2)
+{
+    if(count % 2 === 0)
+    {
+        console.log(count)
+    }
+    else
+    {
+        console.log("noteven")
+    }
+}*/
+
+/*for(count=1;count<=10;count=count+1)
+   {
+    if(count%2!==0)
+    {
+        console.log(count)
+    }
+   }*/
+
+   /* for(count=1;count<=10;count=count+1)
+    {
+        console.log(count+"x2="+count*2)
+    }*/
+   
+ 
